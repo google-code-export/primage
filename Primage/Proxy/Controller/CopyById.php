ccc9d5d3a4adc706b93f577a96a485ddd002b694
barbushin <barbushin@3410b948-9393-fd07-c804-9fcbaaf7bef0>
2010-08-12 09:22:14 +0000

    Added: Primage_Proxy_Controller_CopyWithResize

--- Primage/Proxy/Controller/CopyById.php
+++ Primage/Proxy/Controller/CopyById.php
@@ -2,7 +2,14 @@
 
 class Primage_Proxy_Controller_CopyById extends Primage_Proxy_Controller_Abstract {
 	
+	/**
+	 * @var Primage_Proxy_Storage
+	 */
 	protected $srcStorage;
+	
+	/**
+	 * @var Primage_Proxy_Storage
+	 */
 	protected $dstStorage;
 
 	public function __construct(Primage_Proxy_Storage $srcStorage, Primage_Proxy_Storage $dstStorage) {
@@ -10,22 +17,18 @@ class Primage_Proxy_Controller_CopyById extends Primage_Proxy_Controller_Abstrac
 		$this->dstStorage = $dstStorage;
 	}
 
-	/**
-	 * @param array $params
-	 * @return Primage
-	 */
-	public function dispatch(array $params, $sendResultToStdout = true) {
+	protected function getImageByParams(array $params) {
 		if(empty($params['id'])) {
 			throw new Exception('Parameter "id" is required');
 		}
-		$image = $this->srcStorage->getImage($params['id']);
-		$this->makeActionsOnImage($image);
+		return $this->srcStorage->getImage($params['id']);
+	}
+
+	/**
+	 * @param Primage $image
+	 * @param array $params
+	 */
+	protected function postDispatch(Primage $image, array $params) {
 		$this->dstStorage->storeImage($image, basename($_SERVER['REQUEST_URI']));
-		
-		if($sendResultToStdout) {
-			$image->sendToStdout($this->dstStorage->imageType, $this->dstStorage->imageQuality);
-		}
-		
-		return $image;
 	}
 }
